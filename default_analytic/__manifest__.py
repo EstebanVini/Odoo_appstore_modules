66370fb063b1279f1921a7d0002041a2947b5ed6
Esteban Viniegra <esteban@eviniegra.software>
2025-11-27 10:02:16 +0100

    cambios

--- default_analytic/__manifest__.py
+++ default_analytic/__manifest__.py
@@ -1,5 +1,5 @@
 {
-    'name': 'default_analytic',
+    'name': 'Default Analytic Account by User and Company',
     'version': '18.0.1.0.1',
     'summary': 'Set default analytic accounts for users and companies',
     'author': 'Esteban Viniegra | Pridecta',
